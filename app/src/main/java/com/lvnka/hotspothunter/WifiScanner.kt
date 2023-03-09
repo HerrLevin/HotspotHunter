3ab4938a603d594e344325d29d7977d6dfb78426
Levin Herr <github@17d.me>
2023-03-09 23:39:07 +0100

    add location receiver

--- app/src/main/java/com/lvnka/hotspothunter/WifiScanner.kt
+++ app/src/main/java/com/lvnka/hotspothunter/WifiScanner.kt
@@ -4,10 +4,15 @@ import android.Manifest
 import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
+import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.core.app.ActivityCompat
@@ -16,6 +21,8 @@ class WifiScanner(activity: Activity) : BroadcastReceiver() {
 
     private var resultList = ArrayList<ScanResult>()
     private lateinit var wifiManager: WifiManager
+    private var locationManager: LocationManager
+    private lateinit var location: String
     private var activity: Activity
     val requestLocationPermission = 1
     val requestWifiPermission = 1
@@ -27,6 +34,7 @@ class WifiScanner(activity: Activity) : BroadcastReceiver() {
             this.wifiManager = this.activity.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
             Log.d("TESTING", " wifiManager created")
         }
+        this.locationManager = this.activity.applicationContext.getSystemService(LOCATION_SERVICE) as LocationManager
     }
 
     fun isScanning(): Boolean {
@@ -62,6 +70,7 @@ class WifiScanner(activity: Activity) : BroadcastReceiver() {
     }
 
     fun startScanning() {
+        this.getLocation()
         if (checkWifiPermission()) {
             this.isScanning = true
             Log.d("TESTING", "starting scanning?")
@@ -71,6 +80,30 @@ class WifiScanner(activity: Activity) : BroadcastReceiver() {
                 stopScanning()
             }, 10000)
         }
+    }
+
+    fun getLocation() {
+        if (checkWifiPermission()) {
+            Log.d("LOCATION", "fetch location")
+            this.locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0L,
+                0f,
+                this.locationListener
+            )
+        }
+    }
+
+    private var locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            this@WifiScanner.location = "${location.longitude}:${location.latitude}"
+            Log.d("LOCATION", this@WifiScanner.location)
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 
     fun stopScanning() {
