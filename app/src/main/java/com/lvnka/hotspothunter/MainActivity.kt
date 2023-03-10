0617c67e4b9e0284a4ac4abe96fbb6781deba7d9
Levin Herr <github@17d.me>
2023-03-10 13:16:38 +0100

    Settings mockup

--- app/src/main/java/com/lvnka/hotspothunter/MainActivity.kt
+++ app/src/main/java/com/lvnka/hotspothunter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lvnka.hotspothunter
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -45,7 +46,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val i = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivity(i)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
